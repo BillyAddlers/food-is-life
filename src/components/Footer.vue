1d3abbf7d2068acff967d0d24a4c700c8b17d195
Riichi Rusdiana <finnsonalca123@gmail.com>
2021-09-24 01:19:59 +0700

    Resimpling footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,22 +3,23 @@
         <footer>
             <div class="jumbotron">
                 <div class="row">
-                    <div class="col-sm-6">
+                    <div class="col" align="center">
                         Created with ❤️ by <a href="https://github.com/BillyAddlers">Riichi Rusdiana</a>.
                     </div>
-                    <div class="col-sm-3">
+
+                    <!-- <div class="col-sm-3">
                         <ul>
                             <li><a></a><nuxt-link to="/">Home</nuxt-link></li>
                             <li><a href="https://github.com/BillyAddlers/food-is-life">GitHub</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-3">
-
-                    </div>
+                        
+                    </div> -->
                 </div>
-                <span title="Made by github/BillyAddlers. Click over (C) to see my profile!">
+                <!-- <span title="Made by github/BillyAddlers. Click over (C) to see my profile!">
                     <a class="no-padding"><a class="no-padding" href="https://github.com/BillyAddlers">©</a> 2021-{{yearNow}} Riichi Rusdiana | Food is Life. All right reserved.</a>
-                </span>
+                </span> -->
             </div>
         </footer>
     </div>
